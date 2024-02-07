3126995aa9dee1454dcf6d7b62bbe8b8e8f237a7
N.DZL <cxnt48@gmail.com>
2024-02-07 17:54:50 +0100

    Add project files.

---
+++ OEMInfo-MAUI-Sample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OEMInfo_MAUI_Sample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ OEMInfo-MAUI-Sample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OEMInfo_MAUI_Sample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
